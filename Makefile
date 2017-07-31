677dc0a7d053d05398b2b631e874d634bbe11d60
CharlieSkelton-Kx <charlie@kx.com>
2017-07-31 13:23:02 +0200

    added Swing GridViewer

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all:
 	mkdir -p build/classes
-	javac -d build/classes src/kx/c.java src/kx/examples/Feed.java src/kx/examples/QueryResponse.java src/kx/examples/Subscriber.java src/kx/examples/Server.java src/kx/examples/TypesMapping.java
+	javac -d build/classes src/kx/c.java src/kx/examples/Feed.java src/kx/examples/QueryResponse.java src/kx/examples/Subscriber.java src/kx/examples/Server.java src/kx/examples/TypesMapping.java src/kx/examples/GridViewer.java
 #	jar cf build/c.jar build/kx/*.class
 doc:
 	javadoc -d docs src/kx/c.java 
@@ -14,4 +14,7 @@ Server:
 	java -cp build/classes kx.examples.Server
 TypesMapping:
 	java -cp build/classes kx.examples.TypesMapping
+
+GridViewer:
+	java -cp build/classes kx.examples.GridViewer
 	
---
+++ src/kx/examples/GridViewer.java
@@ -0,0 +1,66 @@
+package kx.examples;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
+import kx.c;
+
+public class GridViewer {
+    public static class KxTableModel extends AbstractTableModel {
+        private c.Flip flip;
+        public void setFlip(c.Flip data) {
+            this.flip = data;
+        }
+
+        public int getRowCount() {
+            return Array.getLength(flip.y[0]);
+        }
+
+        public int getColumnCount() {
+            return flip.y.length;
+        }
+
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            return c.at(flip.y[columnIndex], rowIndex);
+        }
+
+        public String getColumnName(int columnIndex) {
+            return flip.x[columnIndex];
+        }
+    };
+
+    public static void main(String[] args) {
+        KxTableModel model = new KxTableModel();
+        c c = null;
+        try {
+            c = new c("localhost", 5001,"username:password");
+            String query="([]date:.z.D;time:.z.T;sym:10?`8;price:`float$10?500.0;size:10?100)";
+//          String query="0!select last price by sym from trade where date=last date";
+            model.setFlip((c.Flip) c.k(query));
+            JTable table = new JTable(model);
+            table.setGridColor(Color.BLACK);
+            String title = "kdb+ Example - "+model.getRowCount()+" Rows";
+            JFrame frame = new JFrame(title);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.getContentPane().add(new JScrollPane(table), BorderLayout.CENTER);
+            frame.setSize(300, 300);
+            frame.setVisible(true);
+        } catch (Exception ex) {
+            Logger.getLogger(GridViewer.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (c != null) {
+                try {
+                    c.close();
+                }
+                catch (IOException ex) {}
+            }
+        }
+    }
+}
